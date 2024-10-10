e36d37921dc71ce5aed7f1faed054b0c0d3eb718
Manuel Heredia <Spectrasonic117@gmail.com>
2024-10-10 10:26:00 -0600

    💈 - Cambios esteticos al codigo
    agregados tambien aliases al comando givehead

--- src/main/java/com/spectrasonic/givehead/Main.java
+++ src/main/java/com/spectrasonic/givehead/Main.java
@@ -11,20 +11,21 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         this.getCommand("givehead").setExecutor(new GiveHeadCommand(this));
-        getLogger().info(divider);
-        getLogger().info("");
-        getLogger().info(ChatColor.GREEN + "GiveHead plugin enabled!");
-        getLogger().info(ChatColor.GOLD + "Developed by " + ChatColor.RED + "Spectrasonic");
-        getLogger().info("");
-        getLogger().info(divider);
+        
+        getServer().getConsoleSender().sendMessage(divider);
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(prefix + ChatColor.GREEN + "GiveHead plugin enabled!");
+        getServer().getConsoleSender().sendMessage(prefix + ChatColor.GOLD + "Developed by " + ChatColor.RED + "Spectrasonic");
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(divider);
     }
 
     @Override
     public void onDisable() {
-        getLogger().info(divider);
-        getLogger().info("");
-        getLogger().info(ChatColor.RED + "GiveHead plugin Disabled successfully!");
-        getLogger().info("");
-        getLogger().info(divider);
-    }
+
+        getServer().getConsoleSender().sendMessage(divider);
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(prefix + ChatColor.RED + "GiveHead plugin Disabled successfully!");
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(divider);
 }
