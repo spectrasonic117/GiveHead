1f6df68da185373b0ae6b85dfe76c3727efe05d1
Manuel Heredia <Spectrasonic117@gmail.com>
2024-10-10 10:50:40 -0600

    Cambio de version 1.20.4 (Experimental)
    solo es un cambio para que pueda funcionar en esta version, no será soportada en el futuro

--- src/main/java/com/spectrasonic/givehead/Main.java
+++ src/main/java/com/spectrasonic/givehead/Main.java
@@ -6,15 +6,18 @@ import org.bukkit.ChatColor;
 
 public class Main extends JavaPlugin {
     private static final String divider = "--------------------";
-    private static final String prefix = ChatColor.AQUA + "[GiveHead]"+ ChatColor.RESET + " ";
+    private static final String prefix = ChatColor.AQUA + "[GiveHead]" + ChatColor.RESET + " ";
+
+    private final String version = getDescription().getVersion();
 
     @Override
     public void onEnable() {
         this.getCommand("givehead").setExecutor(new GiveHeadCommand(this));
-        
+
         getServer().getConsoleSender().sendMessage(divider);
         getServer().getConsoleSender().sendMessage("");
         getServer().getConsoleSender().sendMessage(prefix + ChatColor.GREEN + "GiveHead plugin enabled!");
+        getServer().getConsoleSender().sendMessage(prefix + ChatColor.LIGHT_PURPLE + "Version: " + ChatColor.AQUA + version);
         getServer().getConsoleSender().sendMessage(prefix + ChatColor.GOLD + "Developed by " + ChatColor.RED + "Spectrasonic");
         getServer().getConsoleSender().sendMessage("");
         getServer().getConsoleSender().sendMessage(divider);
@@ -28,4 +31,5 @@ public class Main extends JavaPlugin {
         getServer().getConsoleSender().sendMessage(prefix + ChatColor.RED + "GiveHead plugin Disabled successfully!");
         getServer().getConsoleSender().sendMessage("");
         getServer().getConsoleSender().sendMessage(divider);
+    }
 }
